a24acb4f6f808733f7f489542ad19e93bc7b73b6
xwteam <xwteam@xwteam.cn>
2025-12-03 10:45:46 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -409,7 +409,7 @@ const fetchDateInfo = async () => {
   try {
     console.log('开始获取日期信息...');
     // 使用简单请求，不设置自定义头部，避免触发CORS预检
-    const response = await fetch('https://api.xwteam.cn/api/time/almanac?key=aOxWVWCoyTFGsBTstbPmXySBp0');
+    const response = await fetch('https://free.xwteam.cn/api/time/almanac?key=aOxWVWCoyTFGsBTstbPmXySBp0');
     console.log('API响应状态:', response.status);
     
     const data = await response.json();
